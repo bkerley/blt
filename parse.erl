b622748b05f3aa7a4bc7d05015d6b69d6c268a93
Bryce Kerley <bkerley@brycekerley.net>
2008-06-22 20:30:44 -0400

    errors handled

--- parse.erl
+++ parse.erl
@@ -6,7 +6,7 @@ parse (Line) when is_binary(Line) ->
 	parse(binary_to_list(Line));
 parse (Line) when is_list(Line)->
 	Normalized = phase0(Line),
-	{ok, [PhaseOne]} = phase1(Normalized, []),
+	PhaseOne = phase1(Normalized),
 	PhaseTwo = phase2(PhaseOne, [], []),
 	PhaseThree = phase3(PhaseTwo, []),
 	PhaseThree.
@@ -19,21 +19,36 @@ phase0(Unnormalized) ->
 			Char
 	end, Unnormalized).
 
-phase1([$(|Remains], List) ->
-	{Postlist, InnerList} = phase1(Remains, []),
-	phase1(Postlist, [InnerList|List]);
+phase1(Normalized) ->
+	try
+		{ok, [Listified]} = listify(Normalized, []),
+		Listified
+	catch
+		error: {badmatch, _} ->
+			erlang:error(
+				{parse, closed_list, "Parse error: tried to close more lists than were opened."})
+	end.
 
-phase1([$)|Remains], List) ->
+listify([], List) ->
+	{ok, List};
+
+listify(ok, _) ->
+	erlang:error(
+		{parse, open_list,"Parse error: ran into the end of line with an open list."});
+
+listify([$(|Remains], List) ->
+	{Postlist, InnerList} = listify(Remains, []),
+	listify(Postlist, [InnerList|List]);
+
+listify([$)|Remains], List) ->
 	{Remains, lists:reverse(List)};
 
-phase1([Current|Remains], List) ->
-	phase1(Remains, [Current|List]);
-
-phase1([], List) ->
-	{ok, List}.
+listify([Current|Remains], List) ->
+	listify(Remains, [Current|List]).
 
 phase2([], [], List) ->
 	lists:reverse(List);
+
 phase2([], Atom, List) ->
 	lists:reverse([lists:reverse(Atom)|List]);
 
@@ -50,14 +65,15 @@ phase2([Char|Remains], Atom, List) when is_integer(Char) ->
 	phase2(Remains, [Char|Atom], List).
 
 phase3([], List) ->
-	lists:reverse(List);
+	{sublist, lists:reverse(List)};
 
 phase3([Sample|Remains], _) when is_integer(Sample) ->
 	try_integerize([Sample|Remains]);
 
 phase3([Sample|Remains], List) when is_list(Sample) ->
 	Sublist = phase3(Sample, []),
-	phase3(Remains, [Sublist|List]).
+	Recurlist = [Sublist|List],
+	phase3(Remains, Recurlist).
 
 try_integerize(Candidate) ->
 	try
