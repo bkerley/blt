42ef645372ddadf072aaafe4c00e07a2950fa373
Bryce Kerley <bkerley@brycekerley.net>
2008-06-23 20:56:47 -0400

    fix +

--- builtins.erl
+++ builtins.erl
@@ -4,7 +4,14 @@
 
 init_builtins() ->
 	define({atom, "list"}, fun(Args) -> {list, Args} end),
-	define({atom, "+"}, fun(Args) -> {number, lists:sum(Args)} end),
+	define({atom, "+"}, fun(Args) ->
+		{number, 
+			lists:foldl(fun
+				({number, N}, Accum) ->
+					Accum + N
+			end,
+			0, Args)
+		} end),
 	define({atom, "cons"}, fun(Args) ->
 		[First,{list, Rest}] = Args,
 		{list, [First|Rest]} end).
